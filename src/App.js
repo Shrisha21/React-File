657f1efbfd6f5ad8d783fdbb4ddb4af5755fbf74
Ideal <shrishadiga1@gmail.com>
2020-06-25 12:08:43 +0530

    Half code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import React, { Component } from 'react'
+import Dropzone from 'react-dropzone';
+import Table from 'react-bootstrap/Table'
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      filesArray:[]
+    }
+  }
+  fileHandle = (file) => {
+    this.setState({
+      filesArray:this.state.filesArray.concat(file)
+    })
+    console.log(this.state.filesArray)
+  }
+  render() {
+    return (
+        <div className='App'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className='center'>
+          React File Upload
+        </div>
+        <br/>
+        <div className='dropzone'>
+        <Dropzone onDrop={acceptedFiles => this.fileHandle(acceptedFiles)} 
+        noClick={true}
+        noKeyboard={true}
         >
-          Learn React
-        </a>
-      </header>
+          {({getRootProps, getInputProps,open}) => (
+        <section>
+          <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            <p>Drag 'n' drop some files here, or click to select files</p>
+            <button style={{color:'green'}} type="button" onClick={open}>
+              Open File Dialog
+        </button>
+          </div>
+        </section>
+      )}
+    </Dropzone>
     </div>
-  );
+    <Table striped bordered hover variant="dark" align='center'>
+      <thead>
+        <tr>
+        <td>File name</td>
+        <td>File size</td>
+        <td>Actions</td>
+        </tr>
+      </thead>
+      <tbody>
+      <tr>
+      {
+        this.state.filesArray.map((key,file)=>
+        <td key={key}>{file.name}</td>)
+      }
+    </tr>
+      </tbody>
+    </Table>
+    </div>
+    )
+  }
 }
 
-export default App;
